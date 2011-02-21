97f955abe6b20e54d46fa7cc31d37f0dd4d5e02f
Martin Kühl <martin.kuehl@gmail.com>
2011-02-21 20:04:27 +0100

    New formula: `abcl`
    ABCL is a full implementation of the Common Lisp language featuring
    both an interpreter and a compiler, running in the JVM.

--- Library/Formula/abcl.rb
+++ Library/Formula/abcl.rb
@@ -2,16 +2,27 @@ require 'formula'
 
 class Abcl < Formula
   homepage 'http://common-lisp.net/project/armedbear/'
-  url 'http://common-lisp.net/project/armedbear/releases/1.0.1/abcl-bin-1.0.1.tar.gz'
+  version '1.0.1'
+  url "http://common-lisp.net/project/armedbear/releases/#{version}/abcl-bin-#{version}.tar.gz"
   md5 'bd95a55df30469b4f6d85af7c5ede297'
+  head 'svn://common-lisp.net/project/armedbear/svn/trunk/abcl'
 
-  depends_on 'rlwrap'
+  def script
+<<-EOS
+#!/bin/sh
+# Run Armed Bear Common Lisp.
+
+# Add ABCL jar to CLASSPATH.
+ABCL="${CLASSPATH:+$CLASSPATH:}#{libexec}/#{name}.jar:#{libexec}/#{name}-contrib.jar"
+
+exec java -cp "$ABCL" org.armedbear.lisp.Main "$@"
+EOS
+  end
 
   def install
-    prefix.install "abcl.jar", "abcl-contrib.jar"
-    (bin+"abcl").write <<-EOS.undent
-      #!/bin/sh
-      rlwrap java -jar "#{prefix}/abcl.jar" "$@"
-    EOS
+    system "ant" if ARGV.build_head?
+    libexec.install 'abcl.jar'
+    libexec.install 'abcl-contrib.jar'
+    (bin+'abcl').write script
   end
 end
