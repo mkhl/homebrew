ee99df144d684472bddc9ed93cd7a9fcdc56428c
Martin Kühl <martin.kuehl@gmail.com>
2011-02-21 23:29:15 +0100

    New formula: `kawa`
    Kawa is:
    * A framework written in Java for implementing high-level and dynamic
      languages, compiling them into Java bytecodes.
    * An implementation of Scheme, which is in the Lisp family of programming
      languages. Kawa is a featureful dialect in its own right, and additionally
      provides very useful integration with Java. It can be used as a “scripting
      language”, but includes a compiler and all the benefits of a “real”
      programming language, including optional static typing.
    * Implementations of other programming languages, including XQuery (Qexo) and
      Emacs Lisp (JEmacs).

--- Library/Formula/kawa.rb
+++ Library/Formula/kawa.rb
@@ -1,17 +1,26 @@
 require 'formula'
 
 class Kawa < Formula
+  homepage 'http://www.gnu.org/software/kawa/'
   url 'http://ftpmirror.gnu.org/kawa/kawa-1.11.jar'
   mirror 'http://ftp.gnu.org/gnu/kawa/kawa-1.11.jar'
-  homepage 'http://www.gnu.org/software/kawa/'
   md5 'eee96e13a329513a2f705ee03bacef63'
+  head 'svn://sourceware.org/svn/kawa/trunk'
+
+  def script
+<<-EOS
+#!/bin/sh
+# Run Kawa Scheme.
+
+# Add Kawa jar to CLASSPATH.
+KAWA="${CLASSPATH:+$CLASSPATH:}#{prefix}/#{name}.jar"
+
+exec java -cp "$KAWA" kawa.repl "$@"
+EOS
+  end
 
   def install
-    prefix.install "kawa-#{version}.jar"
-    (bin+'kawa').write <<-EOS.undent
-      #!/bin/sh
-      KAWA_HOME="#{prefix}"
-      java -jar "$KAWA_HOME/kawa-#{version}.jar"
-    EOS
+    prefix.install "kawa-#{version}.jar" => "kawa.jar"
+    (bin+'kawa').write script
   end
 end
