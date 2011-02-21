6194b5a820c627a279a5fe27a303455f0d92b329
Martin Kühl <martin.kuehl@gmail.com>
2011-02-21 23:29:15 +0100

    New formula: `objc-dep`
    Graph the import dependancies in an Objective-C project.

---
+++ Library/Formula/objc-dep.rb
@@ -0,0 +1,7 @@
+require 'formula'
+
+class ObjcDep <ScriptFileFormula
+  homepage 'https://github.com/nst/objc_dep/'
+  head 'https://github.com/nst/objc_dep/raw/master/objc-dep.py'
+  md5 'f008fc92e51ce3e64bc399fb1624a41b'
+end
