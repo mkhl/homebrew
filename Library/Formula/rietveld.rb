c7ee86359a14adae63f3a18c3d45afdd7fbe0fb8
Martin Kühl <martin.kuehl@gmail.com>
2011-03-13 16:16:04 +0100

    New formula: rietveld

---
+++ Library/Formula/rietveld.rb
@@ -0,0 +1,6 @@
+require 'formula'
+
+class Rietveld <ScriptFileFormula
+  homepage 'http://code.google.com/p/rietveld/wiki/CodeReviewHelp'
+  head 'http://codereview.appspot.com/static/upload.py'
+end
